2aee693ec944262d655d83bd2877109308be5137
Aashutosh Kumar <aashutoshkumar6729@gmail.com>
2025-09-07 00:43:19 +0530

    Swagger dependendy added and filter added in payment processing

--- batch/src/main/java/com/spring/batch/processor/TransactionProcessor.java
+++ batch/src/main/java/com/spring/batch/processor/TransactionProcessor.java
@@ -1,6 +1,9 @@
 package com.spring.batch.processor;
 
 import com.spring.batch.paymentEntity.Transaction;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ValidationException;
+import jakarta.validation.Validator;
 import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -8,16 +11,28 @@ import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.stereotype.Component;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Component
 @RequiredArgsConstructor
 public class TransactionProcessor implements ItemProcessor<Transaction, Transaction> {
 
     private static final Logger log = LogManager.getLogger(TransactionProcessor.class);
-    private final FlatFileItemReader<Transaction> indiaPopulationCensusReader;
+    private final Validator beanValidator;
+
 
     // This method processes each item (in this case each PopulationCensusIndia object) that is read by the reader
     @Override
     public Transaction process(Transaction item) throws Exception {
+        Set<ConstraintViolation<Transaction>> v = beanValidator.validate(item);
+        if (!v.isEmpty()) {
+            String msg = v.stream().map(ConstraintViolation::getMessage).collect(Collectors.joining("; "));
+            throw new ValidationException("Bean validation failed: " + msg);
+        }
+        if (!Set.of("INR","USD","EUR").contains(item.getCurrency())) {
+            throw new IllegalArgumentException("Unsupported currency: " + item.getCurrency());
+        }
         log.info(item);
         return item; // Return the cleaned item
     }
